670311bfd4ad6af69725f2f0c2d01c7a61fc157a
Christoph <cgehbauer@lbl.gov>
2022-07-15 12:56:07 -0700

    fixed init for setup.py

--- doper/__init__.py
+++ doper/__init__.py
@@ -2,7 +2,10 @@
 This is the DOPER main module.
 """
 
-from .utility import *
-from .wrapper import *
+try:
+    from .utility import *
+    from .wrapper import *
+except Exception as e:
+    print(f'ERROR importing internal DOPER functions:\n{e}')
 
 __version__ = "0.2.1"
