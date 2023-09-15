fdd6ebb2f560e8c584f5ed4a712c788cb08f0934
Christoph <cgehbauer@lbl.gov>
2023-09-15 15:15:05 -0700

    fixed solver installaiton on Windows

--- doper/solvers/setup_solvers.bat
+++ doper/solvers/setup_solvers.bat
@@ -1,7 +1,7 @@
 if [%1]==[] (set solver_dir="../solvers/") else (set solver_dir=%1)
 
-set cbc_repo="https://github.com/coin-or/Cbc/releases/download/releases%2F"
-set cbc_version="2.10.8"
+set cbc_repo=https://github.com/coin-or/Cbc/releases/download/releases%%2F
+set cbc_version=2.10.8
 
 set solvers=(cbc)
 cd $solver_dir
@@ -9,7 +9,8 @@ cd $solver_dir
 mkdir Windows64
 cd Windows64
 for %%x in %solvers% do (
-	curl %cbc_repo%%cbc_version%/Cbc-releases.%cbc_version%-i686-w64-mingw32.zip -O -J -L
-	tar -xf %%x-win64.zip
-	del %%x-win64.zip
+	curl "%cbc_repo%%cbc_version%/Cbc-releases.%cbc_version%-i686-w64-mingw32.zip" -O -J -L
+	tar -xf Cbc-releases.%cbc_version%-i686-w64-mingw32.zip
+    MOVE bin\*
+	del Cbc-releases.%cbc_version%-i686-w64-mingw32.zip
 )
