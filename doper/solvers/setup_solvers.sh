1172ea56d67ad1d2193ef80265b96c9156f18173
Christoph <cgehbauer@lbl.gov>
2026-05-20 15:40:28 -0700

    fixed install bug vscode

--- doper/solvers/setup_solvers.sh
+++ doper/solvers/setup_solvers.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 if [ "$1" ]; then
     solver_dir=$1
 else
@@ -8,16 +10,19 @@ cbc_repo="https://github.com/coin-or/Cbc/releases/download/releases%2F"
 cbc_version="2.10.8"
 
 solvers="cbc"
-cd $solver_dir
 
-mkdir Linux64
-cd Linux64
+cd "$solver_dir" || exit 1
+
+mkdir -p Linux64
+cd Linux64 || exit 1
+
 for s in $solvers
 do
-    fname=Cbc-releases.${cbc_version}-x86_64-ubuntu18-gcc750-static.tar.gz
-	wget ${cbc_repo}${cbc_version}/${fname}
-    tar -xvzf ${fname}
-    rm ${fname}
-	mv bin/* .
+    fname="Cbc-releases.${cbc_version}-x86_64-ubuntu18-gcc750-static.tar.gz"
+    wget "${cbc_repo}${cbc_version}/${fname}"
+    tar -xvzf "${fname}"
+    rm -f "${fname}"
+    mv bin/* .
 done
+
 cd ..
