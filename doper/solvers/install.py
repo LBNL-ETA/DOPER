57467aaff4651d1eac42b0327d0be95138feda16
Christoph <cgehbauer@lbl.gov>
2026-05-20 18:11:38 -0700

    verbose install script

--- doper/solvers/install.py
+++ doper/solvers/install.py
@@ -8,31 +8,42 @@ It should be called explicitly after package installation (e.g. via CLI entry po
 import os
 import sys
 import subprocess as sp
+from subprocess import DEVNULL
 
 
-def install_solvers():
+def install_solvers(debug=False):
     """Run bundled solver setup scripts from the installed package location."""
     solvers_dir = os.path.dirname(os.path.abspath(__file__))
-
-    print(f'Installing Solvers in: {solvers_dir}')
+    stdio_kwargs = {} if debug else {'stdout': DEVNULL, 'stderr': DEVNULL}
 
     if 'win' not in sys.platform:
         clean_script = os.path.join(solvers_dir, 'clean_setup_solvers.sh')
         setup_script = os.path.join(solvers_dir, 'setup_solvers.sh')
 
-        clean_result = sp.call(['sh', clean_script], cwd=solvers_dir)
+        clean_result = sp.call(
+            ['sh', clean_script],
+            cwd=solvers_dir,
+            **stdio_kwargs
+        )
         if clean_result != 0:
             raise RuntimeError(f'Solver cleanup failed with exit code {clean_result}.')
 
-        result = sp.call(['sh', setup_script], cwd=solvers_dir)
+        result = sp.call(
+            ['sh', setup_script],
+            cwd=solvers_dir,
+            **stdio_kwargs
+        )
     else:
         setup_script = os.path.join(solvers_dir, 'setup_solvers.bat')
-        result = sp.call(setup_script, shell=True, cwd=solvers_dir)
+        result = sp.call(
+            setup_script,
+            shell=True,
+            cwd=solvers_dir,
+            **stdio_kwargs
+        )
 
     if result != 0:
         raise RuntimeError(f'Solver installation failed with exit code {result}.')
-
-    print('Solver installation done.')
 
 
 if __name__ == '__main__':
